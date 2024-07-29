170ba1ab7203b70bbb18fe935c041ad152280d1e
Ori Shem-Tov <orishemtov@gmail.com>
2024-07-29 12:05:21 +0300

    add comments

--- smart_contracts/proposal/types.py
+++ smart_contracts/proposal/types.py
@@ -2,5 +2,10 @@ import typing
 
 from algopy import arc4
 
+# corresponds to CID_LENGTH in ./constants.py. We cannot use a variable here because as it causes type errors which
+# fails compilation.
 Cid = arc4.StaticArray[arc4.Byte, typing.Literal[59]]
+
+# corresponds to COMMITTEE_ID_LENGTH in ./constants.py. We cannot use a variable here because as it causes type errors
+# which fails compilation.
 CommitteeId = arc4.StaticArray[arc4.Byte, typing.Literal[32]]
