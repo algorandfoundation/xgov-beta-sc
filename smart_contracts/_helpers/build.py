34349d874a293b462e9941b172bf635e96c674f2
cusma <cosimo.bassi@algorand.foundation>
2024-12-12 16:19:07 +0100

    chore: update build.py

--- smart_contracts/_helpers/build.py
+++ smart_contracts/_helpers/build.py
@@ -3,10 +3,16 @@ import subprocess
 from pathlib import Path
 from shutil import rmtree
 
-from smart_contracts._helpers.util import find_app_spec_file
-
 logger = logging.getLogger(__name__)
 deployment_extension = "py"
+
+
+def _get_output_path(output_dir: Path, deployment_extension: str) -> Path:
+    return output_dir / Path(
+        "{contract_name}"
+        + ("_client" if deployment_extension == "py" else "Client")
+        + f".{deployment_extension}"
+    )
 
 
 def build(output_dir: Path, contract_path: Path) -> Path:
@@ -25,7 +31,7 @@ def build(output_dir: Path, contract_path: Path) -> Path:
             contract_path.absolute(),
             f"--out-dir={output_dir}",
             "--output-arc32",
-            "--match-algod-bytecode",
+            "--output-source-map",
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
@@ -34,31 +40,37 @@ def build(output_dir: Path, contract_path: Path) -> Path:
     if build_result.returncode:
         raise Exception(f"Could not build contract:\n{build_result.stdout}")
 
-    app_spec_file_name = find_app_spec_file(output_dir)
-    if app_spec_file_name is None:
-        raise Exception("Could not generate typed client, .arc32.json file not found")
+    app_spec_file_names = [file.name for file in output_dir.glob("*.arc32.json")]
+    app_spec_file_name = None
+    for app_spec_file_name in app_spec_file_names:
+        if app_spec_file_name is None:
+            logger.warning(
+                "No '*.arc32.json' file found (likely a logic signature being compiled). Skipping client generation."
+            )
+            continue
+        print(app_spec_file_name)
+        generate_result = subprocess.run(
+            [
+                "algokit",
+                "generate",
+                "client",
+                output_dir,
+                "--output",
+                _get_output_path(output_dir, deployment_extension),
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+        if generate_result.returncode:
+            if "No such command" in generate_result.stdout:
+                raise Exception(
+                    "Could not generate typed client, requires AlgoKit 2.0.0 or "
+                    "later. Please update AlgoKit"
+                )
+            else:
+                raise Exception(
+                    f"Could not generate typed client:\n{generate_result.stdout}"
+                )
 
-    generate_result = subprocess.run(
-        [
-            "algokit",
-            "generate",
-            "client",
-            output_dir / app_spec_file_name,
-            "--output",
-            output_dir / f"client.{deployment_extension}",
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-    )
-    if generate_result.returncode:
-        if "No such command" in generate_result.stdout:
-            raise Exception(
-                "Could not generate typed client, requires AlgoKit 2.0.0 or "
-                "later. Please update AlgoKit"
-            )
-        else:
-            raise Exception(
-                f"Could not generate typed client:\n{generate_result.stdout}"
-            )
-    return output_dir / app_spec_file_name
+    return output_dir / app_spec_file_name if app_spec_file_name else output_dir
