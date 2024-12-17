9415848b97dc02dcf7df5adccca993fc5d3ae71f
cusma <cosimo.bassi@algorand.foundation>
2024-12-17 18:29:34 +0100

    feat: publish app spec on ipfs

--- smart_contracts/_helpers/deploy.py
+++ smart_contracts/_helpers/deploy.py
@@ -1,6 +1,7 @@
 # mypy: disable-error-code="no-untyped-call, misc"
 
 
+import json
 import logging
 from collections.abc import Callable
 from pathlib import Path
@@ -14,9 +15,12 @@ from algokit_utils import (
     get_algod_client,
     get_indexer_client,
 )
+from algosdk.transaction import PaymentTxn
 from algosdk.util import algos_to_microalgos
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
+
+from . import ipfs
 
 logger = logging.getLogger(__name__)
 
@@ -36,7 +40,7 @@ def deploy(
     # get app spec
     app_spec = ApplicationSpecification.from_json(app_spec_path.read_text())
 
-    # get deployer account by name
+    # get a deployer account by name
     deployer = get_account(algod_client, "DEPLOYER", fund_with_algos=0)
 
     minimum_funds_micro_algos = algos_to_microalgos(deployer_initial_funds)
@@ -51,3 +55,26 @@ def deploy(
 
     # use provided callback to deploy the app
     deploy_callback(algod_client, indexer_client, app_spec, deployer)
+
+    # upload app spec to ipfs
+    logger.info("Uploading AppSpec on IPFS")
+    jwt = ipfs.get_pinata_jwt()
+    app_spec_cid = ipfs.upload_to_pinata(app_spec_path, jwt, app_spec.contract.name)
+    app_spec_url = ipfs.GATEWAY + app_spec_cid
+    logger.info(f"{app_spec.contract.name} AppSpec URL: {app_spec_url}")
+
+    arc2_data: dict[str, str] = {
+        "app-spec-cid": app_spec_cid,
+        "app-spec-url": app_spec_url,
+    }
+    arc2_note = f"{app_spec.contract.name}:j" + json.dumps(arc2_data)
+    app_spec_txn = PaymentTxn(
+        sender=deployer.address,
+        sp=algod_client.suggested_params(),
+        receiver=deployer.address,
+        amt=0,
+        note=arc2_note.encode(),
+    )
+    logger.info(f"Notarizing AppSpec CID on {app_spec_txn.genesis_id}")
+    algod_client.send_transaction(app_spec_txn.sign(deployer.signer.private_key))
+    logger.info(f"{app_spec.contract.name} AppSpec TxnID: {app_spec_txn.get_txid()}")
