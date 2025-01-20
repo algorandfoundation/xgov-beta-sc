a173b8c5e66f645701d7e6c8aa34b434dc49e7e9
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2025-01-20 17:02:52 +0300

    fix: updates cohort CID from 32 to 36

--- smart_contracts/common/types.py
+++ smart_contracts/common/types.py
@@ -8,7 +8,7 @@ Cid = arc4.StaticArray[arc4.Byte, typing.Literal[59]]
 
 # corresponds to COMMITTEE_ID_LENGTH in ./constants.py. We cannot use a variable here because as it causes type errors
 # which fails compilation.
-CommitteeId = arc4.StaticArray[arc4.Byte, typing.Literal[32]]
+CommitteeId = arc4.StaticArray[arc4.Byte, typing.Literal[36]]
 
 
 Error = arc4.String
