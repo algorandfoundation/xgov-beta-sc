92ce93b953dbf61e83460b48fd2bc22a31de4e5e
cusma <cosimo.bassi@algorand.foundation>
2025-07-22 23:52:23 +0200

    chore: reduce initial funds

--- tests/common.py
+++ tests/common.py
@@ -1,7 +1,6 @@
 from typing import Final
 from algokit_utils import AlgoAmount
 
-from smart_contracts.xgov_registry.config import MAX_REQUESTED_AMOUNT_LARGE
 from smart_contracts.proposal.constants import (
     BPS,
     COMMITTEE_ID_LENGTH,
@@ -15,4 +14,4 @@ DEFAULT_COMMITTEE_ID = b"\x01" * COMMITTEE_ID_LENGTH
 DEFAULT_COMMITTEE_MEMBERS = 20
 DEFAULT_COMMITTEE_VOTES = 200
 
-INITIAL_FUNDS: Final[AlgoAmount] = AlgoAmount(micro_algo=MAX_REQUESTED_AMOUNT_LARGE + 1_000)
+INITIAL_FUNDS: Final[AlgoAmount] = AlgoAmount(algo=1_000)
