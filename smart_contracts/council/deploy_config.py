c972657925beebfdcfeb6ffa9938f1a7d5437676
Kyle Breeding <kyle.breeding@algorand.foundation>
2025-11-05 13:27:03 -0800

    chore: lint fixes

--- smart_contracts/council/deploy_config.py
+++ smart_contracts/council/deploy_config.py
@@ -3,6 +3,11 @@ import os
 
 from algokit_utils import AlgoAmount, AlgorandClient, OnSchemaBreak, OnUpdate
 
+from smart_contracts.artifacts.council.council_client import (
+    CouncilMethodCallCreateParams,
+    CreateArgs,
+)
+
 from ..xgov_registry.deploy_config import _create_vault_signer_from_env
 
 logger = logging.getLogger(__name__)
@@ -16,7 +21,6 @@ council_min_spending = AlgoAmount.from_algo(2)
 def deploy() -> None:
     from smart_contracts.artifacts.council.council_client import (
         CouncilFactory,
-        CreateArgs,
     )
 
     algorand_client = AlgorandClient.from_environment()
@@ -55,25 +59,17 @@ def deploy() -> None:
     client, result = factory.deploy(
         on_update=OnUpdate.AppendApp,
         on_schema_break=OnSchemaBreak.AppendApp,
-        create_params=CreateArgs(
-            admin=admin_address,
-            registry_id=registry_app_id,
+        create_params=CouncilMethodCallCreateParams(
+            args=CreateArgs(
+                registry_id=registry_app_id,
+            ),
         ),
     )
 
     logger.info(f"Council deployed with app ID: {client.app_id}")
     logger.info(f"Operation performed: {result.operation_performed}")
 
-    # Fund the council app if it was created
-    if result.operation_performed in ["Create", "Replace"]:
-        algorand_client.send.payment(
-            sender=deployer_address,
-            receiver=client.app_address,
-            amount=council_min_spending,
-            signer=(
-                vault_signer
-                if vault_signer
-                else (gh_deployer.signer if gh_deployer else None)
-            ),
-        )
-        logger.info("Funded council app with 0.1 ALGO")
+    algorand_client.account.ensure_funded_from_environment(
+        account_to_fund=client.app_address,
+        min_spending_balance=council_min_spending,
+    )
