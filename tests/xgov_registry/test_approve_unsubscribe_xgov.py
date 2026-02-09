5ba5464f7cfdb9aa3199b2324f3f536dd3d85b09
cusma <cosimo.bassi@algorand.foundation>
2026-02-09 15:44:40 +0100

    test: not xgov

--- tests/xgov_registry/test_approve_unsubscribe_xgov.py
+++ tests/xgov_registry/test_approve_unsubscribe_xgov.py
@@ -3,18 +3,21 @@ from algokit_utils import (
     AlgorandClient,
     CommonAppCallParams,
     LogicError,
+    PaymentParams,
     SigningAccount,
 )
 from algosdk.error import AlgodHTTPError
 
 from smart_contracts.artifacts.xgov_registry.x_gov_registry_client import (
     ApproveUnsubscribeXgovArgs,
+    RequestUnsubscribeXgovArgs,
     XGovRegistryClient,
 )
 from smart_contracts.artifacts.xgov_subscriber_app_mock.x_gov_subscriber_app_mock_client import (
     XGovSubscriberAppMockClient,
 )
 from smart_contracts.errors import std_errors as err
+from tests.xgov_registry.common import get_xgov_fee
 
 
 def test_approve_unsubscribe_xgov_success(
@@ -57,5 +60,37 @@ def test_approve_unsubscribe_xgov_not_subscriber(
         )
 
 
-def test_approve_unsubscribe_xgov_not_xgov() -> None:
-    pass  # TODO
+def test_approve_unsubscribe_xgov_not_xgov(
+    algorand_client: AlgorandClient,
+    xgov_subscriber: SigningAccount,
+    xgov_registry_client: XGovRegistryClient,
+    xgov: SigningAccount,
+) -> None:
+    xgov_registry_client.send.request_unsubscribe_xgov(
+        args=RequestUnsubscribeXgovArgs(
+            xgov_address=xgov.address,
+            owner_address=xgov.address,
+            relation_type=0,
+            payment=algorand_client.create_transaction.payment(
+                PaymentParams(
+                    sender=xgov.address,
+                    receiver=xgov_registry_client.app_address,
+                    amount=get_xgov_fee(xgov_registry_client),
+                )
+            ),
+        ),
+        params=CommonAppCallParams(sender=xgov.address),
+    )
+
+    request_id = xgov_registry_client.state.global_state.request_id - 1
+
+    # Unsubscribe the xGov directly (bypassing the request system, this is not possible in production)
+    xgov_registry_client.send.unsubscribe_xgov(
+        params=CommonAppCallParams(sender=xgov.address),
+    )
+
+    with pytest.raises(LogicError, match=err.NOT_XGOV):
+        xgov_registry_client.send.approve_unsubscribe_xgov(
+            args=ApproveUnsubscribeXgovArgs(request_id=request_id),
+            params=CommonAppCallParams(sender=xgov_subscriber.address),
+        )
